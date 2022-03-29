f2914c1e589d9202f76837b71df1b1e7dfeca6f9
None <davron328@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2022-03-29 13:33:38 -0500

    added repo

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+from flask import Flask,  jsonify 
+import os 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World"
+@app.route('/')
+def hello_world():
+    return jsonify({
+        'message': 'Welcome to FuchiCorp Hello World app!',
+        'environment': os.environ.get('ENVIRONMENT'),
+    })
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug='True')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
